d15c0b93b3f6c2abda059caf473c02b3db0418e4
albertluk <aluk@uci.edu>
2013-03-10 04:52:34 -0700

    Added trip.js. Use getFinalCalculationMap() to obtain map of Key = person'name and value = amount left to pay
    Signed-off-by: albertluk
    <aluk@uci.edu>

---
+++ js/trip.js
@@ -0,0 +1,82 @@
+
+var amountToPay = {};
+
+function trip(listName, travelList, totalPot)
+{
+
+	this.personList = listName;
+	this.travelList = travelList;
+	this.totalPot = totalPot;
+}
+
+function calculateTripInvoice(allPersonList, travelItemList)
+{
+
+	this.allPersonList = allPersonList;
+	this.travelItemList = travelItemList;
+	
+	var currTotalMap = {};
+	var totalPot = 0;
+
+
+	//Populates and increments overall contributed to the pot
+	for (var i=0; i < travelItemList.length; i++)
+	{ 	
+		for(var j = 0; j < travelItemList[i].personList.length; j++)
+		{
+			
+			if(isNaN(currTotalMap[travelItemList[i].personList[j].name]))
+			{
+				currTotalMap[travelItemList[i].personList[j].name] = travelItemList[i].cost;	
+			}
+			else 
+			{
+				currTotalMap[travelItemList[i].personList[j].name] += travelItemList[i].cost;					
+			}
+
+		}
+		totalPot += travelItemList[i].cost;
+		
+	}
+
+	var averagePerPerson = totalPot/allPersonList.length;
+
+	//Calculates how much is left to pay based on average
+	for(var k = 0; k < allPersonList.length; k++)
+	{
+		
+		
+		if(isNaN(currTotalMap[allPersonList[k].name]))
+		{
+			amountToPay[allPersonList[k].name] = averagePerPerson;
+		}
+		else
+		{
+			amountToPay[allPersonList[k].name] = averagePerPerson - currTotalMap[allPersonList[k].name];
+		}
+	}
+}
+
+function travelItem(itemName, cost, personList)
+{
+	this.itemName = itemName; 
+	this.cost = cost;
+	this.personList = personList;
+}
+
+function getFinalCalculationMap()
+{
+	return amountToPay;
+}
+
+
+//function mapTest(){
+//	var map =  {};
+//	map["hello"] = "sup";
+//	map["ooo"] = "asdf";
+//	delete map["hello"];
+//	alert(map["ooo"]);
+//}
+
+
+
